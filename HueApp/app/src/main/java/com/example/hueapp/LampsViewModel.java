336f463f769e02833076a4097c7d66d7057a4a89
Ewoodss <70382659+Ewoodss@users.noreply.github.com>
2021-11-12 13:48:40 +0100

    als het goed is word de geselcteerde item nu geset in de viewmodel

--- HueApp/app/src/main/java/com/example/hueapp/LampsViewModel.java
+++ HueApp/app/src/main/java/com/example/hueapp/LampsViewModel.java
@@ -1,7 +1,32 @@
 package com.example.hueapp;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class LampsViewModel extends ViewModel {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LampsViewModel extends ViewModel implements LampRecyclerViewAdapter.OnItemClickListener {
     // TODO: Implement the ViewModel
+    private final MutableLiveData<Lamp> selected = new MutableLiveData<>();
+    private static final String LOGTAG = LampsViewModel.class.getName();
+
+    public LiveData<Lamp> getSelected() {
+        return selected;
+    }
+
+    public List<Lamp> getItems()
+    {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void onItemClick(int clickedPosition) {
+        Log.i(LOGTAG, "onItemClick: " + clickedPosition);
+        selected.setValue(getItems().get(clickedPosition));
+    }
 }
